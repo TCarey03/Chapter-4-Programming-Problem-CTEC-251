6ad6603d9417309165d647558d2a702ce458b7f0
TCarey03 <tylercarey03@gmail.com>
2026-09-13 22:25:14 -0500

    Phase 4 completed.

--- Chapter 4 Programming Problem CTEC-251/src/DungeonSpawner.java
+++ Chapter 4 Programming Problem CTEC-251/src/DungeonSpawner.java
@@ -3,12 +3,14 @@ public class DungeonSpawner extends EnemySpawner {
     @Override
     protected Enemy createEnemy(String type) {
 
+        EquipmentFactory equipmentFactory = new MageEquipmentFactory();
+
         switch (type.toLowerCase()) {
             case "skeleton":
-                return new Skeleton();
+                return new Skeleton(equipmentFactory);
 
             case "slime":
-                return new Slime();
+                return new Slime(equipmentFactory);
 
             default:
                 return null;
