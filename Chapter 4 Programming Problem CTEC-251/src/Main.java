c393345364706362f72de22de9f9cecd4b6e4640
TCarey03 <tylercarey03@gmail.com>
2026-09-13 21:47:30 -0500

    Phase 2 completed.
    Refactored enemy creation to use spawners for different environments.

--- Chapter 4 Programming Problem CTEC-251/src/Main.java
+++ Chapter 4 Programming Problem CTEC-251/src/Main.java
@@ -2,12 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SimpleEnemyFactory factory = new SimpleEnemyFactory();
+        EnemySpawner forestSpawner = new ForestSpawner();
+        EnemySpawner dungeonSpawner = new DungeonSpawner();
 
-        Enemy goblin = factory.createEnemy("goblin");
-        Enemy skeleton = factory.createEnemy("skeleton");
-
+        Enemy goblin = forestSpawner.spawnEnemy("goblin");
         goblin.attack();
+
+        Enemy skeleton = dungeonSpawner.spawnEnemy("skeleton");
         skeleton.attack();
+
+        Enemy wolf = forestSpawner.spawnEnemy("wolf");
+        wolf.attack();
+
+        Enemy slime = dungeonSpawner.spawnEnemy("slime");
+        slime.attack();
     }
 }
