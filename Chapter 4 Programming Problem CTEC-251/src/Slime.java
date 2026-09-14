d63e7e7dd02d64b542d4d05401c831c42b75e473
TCarey03 <tylercarey03@gmail.com>
2026-09-13 22:16:21 -0500

    Phase 3 completed.
    Added constructor to Slime class to initialize with EquipmentFactory.

--- Chapter 4 Programming Problem CTEC-251/src/Slime.java
+++ Chapter 4 Programming Problem CTEC-251/src/Slime.java
@@ -1,7 +1,16 @@
 public class Slime extends Enemy {
 
+    public Slime(EquipmentFactory equipmentFactory) {
+        super(equipmentFactory);
+    }
+
     @Override
     public void attack() {
-        System.out.println("Slime attacks by splashing acid!");
+        System.out.println(
+                "Slime attacks with "
+                + weapon.getDescription()
+                + " wearing "
+                + armor.getDescription()
+        );
     }
 }
